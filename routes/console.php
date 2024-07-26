1bc543d0828885fa8f446894315d9f6fb9224fc6
Roihan-Suka-Coding <roihanarrafli@gmail.com>
2024-07-26 21:13:04 +0700

    Add scheduled task to update student SPP on the 1st of every month

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,23 @@
 <?php
 
+use App\Models\Kelas;
+use App\Models\Student;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('update:tagihan_spp', function () {
+    $students = Student::all();
+    foreach ($students as $student) {
+        $kelas = Kelas::find($student->kelas_id);
+
+        $student->tagihan_spp += $kelas->nominal_spp;
+        $student->save();
+    }
+
+    $this->info('Tagihan SPP bulan ini berhasil di update');
+})->description('Tagihan SPP berhasil diperbarui untuk semua siswa')->monthlyOn(1, '00:00');
